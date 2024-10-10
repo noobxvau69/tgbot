34a24612da6b94c29c9836f845dfdf2f6829a393
noobxvau69 <mnshiddik1@gmail.com>
2024-10-10 14:27:30 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    dashboardPassword: "your-password",
+    logType: "tgbot",
+    note: "Change this to 'mongodb', 'tgBot' or 'json' as needed",
+    timezone: "Asia/Dhaka",
+    bot: {
+        token: "telegram_bot_token",
+        id: "7428422156:AAENGD7sie6Bk5aHCRq9cNS_FiH1a27CDWI",
+        note: "Add telegram bot token and chatid for telegram log."
+    },
+    database: {
+        mongodbURL: "mongo_url_here",
+        note: "Add your mongodb database url here"
+    },
+    jsonPath: "./data/submissions.json"
+};
